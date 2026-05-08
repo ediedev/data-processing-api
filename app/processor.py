16cd8194b9eae8486ec2955bf4838105cd33cba3
edtorto <edwardmtsa@gmail.com>
2026-05-08 08:07:45 +0300

    Modify: Modified processor.py to cleaned csv

--- app/processor.py
+++ app/processor.py
@@ -8,11 +8,21 @@ def clean_csv(file_content: bytes):
     # Load into pandas
     df = pd.read_csv(StringIO(s))
 
-    # BASIC CLEANING
+    # Remove duplicates
     df = df.drop_duplicates()
-    df.columns = [col.strip().lower() for col in df.columns]
 
-    # Fill missing values (simple strategy)
+    # Clean column names
+    df.columns = [
+        col.strip().lower().replace(" ", "_")
+        for col in df.columns
+    ]
+
+    # Remove spaces in string values
+    df = df.map(
+        lambda x: x.strip() if isinstance(x, str) else x
+    )
+
+    # Fill missing values
     df = df.fillna("")
 
     return df
